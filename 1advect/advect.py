55adb830ae0ef48fbdcbeccb1ea088483c37afeb
Sudarshan <sudarshan@Sudarshans-iMac.local>
2024-10-10 11:51:02 +0530

    add argparse and save sol

--- 1advect/advect.py
+++ 1advect/advect.py
@@ -1,5 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
+# this computes the solution of advection equation with speed 1
+# ut+ ux = 0 with UW scheme
+
+# Get arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-nc', type=int, help='Number of cells', default=100)
+parser.add_argument('-cfl', type=float, help='CFL number', default=0.9)
+parser.add_argument('-Tf', type=float, help='Final time', default=1e40)
+parser.add_argument('-plot_freq', type=int, help='Frequency to plot solution',
+                    default=1)
+args = parser.parse_args()
 
 def smooth(x):
     return np.sin(2.0*np.pi*x)
@@ -9,8 +21,8 @@ x  = np.linspace(0,1,ng)
 h = 1/(ng-1);
 u  = smooth(x)  # initialize the solution vector
 ue = smooth(x) # exact solution at t =0
-plot_freq = 1
-cfl = 1.0
+plot_freq = args.plot_freq
+cfl = 0.9
 t = 0.0  # initial time
 # plot initial condition
 if plot_freq >0:
@@ -27,7 +39,7 @@ if plot_freq >0:
 
 Tf = 1.0; # final time
 dt = cfl * h   # h is dx
-
+iter  = 0
 while t < Tf:
     lam = dt/h
     if t+dt > Tf:
@@ -35,12 +47,14 @@ while t < Tf:
         lam = dt/h
     # warning! uold = u is a wrong assignment
     uold = np.copy(u)
+    uold[-1] = uold[ng-2] # periodic  boundary condition
+    for i in range(0, ng):
+        u[i] = uold[i] - lam * (uold[i] - uold[i-1])
 
-    for i in range(0, ng-1):
-        u[i] = uold[i] - lam * (uold[i+1] - uold[i])
-
-    u[-1] = u[0];  # periodic  boundary condition
     t +=dt
+    iter +=1
+    print('iter,t, solmin, solmax=',"%04d"% iter, "%.5f"% t, "%.6f"%round(u.min(),6), \
+              "%.7f"%round(u.max(),7) )
     if plot_freq > 0:
         ue = smooth(x-t)
         line1.set_ydata(u)
@@ -51,12 +65,19 @@ while t < Tf:
 plt.show()
 
 ue = smooth(x-t)
+#save solution to a pdf file
 plt.plot(x, u,'ko',fillstyle='none',label='UW')
 plt.plot(x, ue,'-',fillstyle='none',label='Exact')
 plt.xlabel('x')
 plt.ylabel('u')
 plt.legend();
 plt.grid(True, linestyle = '--')
-plt.savefig('u_sapprox.pdf')
+plt.savefig('solution.pdf')
+
+# save solution to a  data file 
+fname1  = 'exact.txt'
+np.savetxt(fname1, np.column_stack([x, ue]))
+fname2 = 'uw.txt'
+np.savetxt(fname2, np.column_stack([x, u]) )
 # u_t + c u_x = 0,  u(x,t ) = g(x-ct)
 # u(x,0) = g(x)   
